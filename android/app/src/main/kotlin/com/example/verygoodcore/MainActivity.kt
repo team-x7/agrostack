bfd53207d509657de6f0bc62a93f4b12c54301cc
sameer pokhre <sameeerpokhrel@gmail.com>
2023-01-21 18:23:26 +0545

    initial setup

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.agrostack
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_production.dart
@@ -0,0 +1,6 @@
+import 'package:agrostack/app/app.dart';
+import 'package:agrostack/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
